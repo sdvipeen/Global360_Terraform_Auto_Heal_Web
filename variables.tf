55c7be50cea48dfe353253e3bf213b8d11a4bf21
Vipeen Sekharan <vsekharan@federalgroup.com.au>
2026-04-15 16:57:22 +1000

    Added variables and tfvars

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default = "Australia Southeast"
+}
+
+variable "resource_group_name" {
+  default = "rg-autoheal-web-dev"
+}
+
+variable "prefix" {
+  default = "autoheal"
+}
